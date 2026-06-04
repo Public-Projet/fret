77f545bdd54e5c0fdc1ca5b707cdb0526aca744f
steveasterafovo <steveasterafovo@gmail.com>
2026-06-04 20:16:29 +0100

    feat: implement home page layout and add partners section with dynamic fetching

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overflow-hidden">
     <RootHomeHero />
-    <RootHomeTrustBar />
+    <RootHomePartners />
     <RootHomeHowItWorks />
     <RootHomeAdvantages />
     <RootHomeTestimonials />
