5128976027b53aa36b780d98ff3cbda12b11fe99
steveasterafovo <steveasterafovo@gmail.com>
2026-06-11 04:38:45 +0100

    feat: implement legal pages including terms, privacy policy, and cookies with a dedicated layout

---
+++ components/layout/Guest/Background.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="absolute inset-0 z-0 overflow-hidden pointer-events-none">
+    <div
+      class="absolute top-0 left-0 w-full h-full bg-[url('/img/grid-pattern.svg')] opacity-[0.03] dark:opacity-[0.02]">
+    </div>
+    <div
+      class="absolute top-0 right-0 w-[500px] h-[500px] bg-primary-500/10 rounded-full blur-[100px] -translate-y-1/2 translate-x-1/2">
+    </div>
+    <div
+      class="absolute bottom-0 left-0 w-[500px] h-[500px] bg-secondary-500/10 rounded-full blur-[100px] translate-y-1/2 -translate-x-1/2">
+    </div>
+  </div>
+</template>
--- layouts/guest.vue
+++ layouts/guest.vue
@@ -2,17 +2,7 @@
   <div
     class="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors duration-200 relative selection:bg-primary-500 selection:text-white">
     <!-- Background Decorator -->
-    <div class="absolute inset-0 z-0 overflow-hidden pointer-events-none">
-      <div
-        class="absolute top-0 left-0 w-full h-full bg-[url('/img/grid-pattern.svg')] opacity-[0.03] dark:opacity-[0.02]">
-      </div>
-      <div
-        class="absolute top-0 right-0 w-[500px] h-[500px] bg-primary-500/10 rounded-full blur-[100px] -translate-y-1/2 translate-x-1/2">
-      </div>
-      <div
-        class="absolute bottom-0 left-0 w-[500px] h-[500px] bg-secondary-500/10 rounded-full blur-[100px] translate-y-1/2 -translate-x-1/2">
-      </div>
-    </div>
+    <LayoutGuestBackground />
 
     <LayoutGuestHeader class="relative z-30" />
     <main class="flex-grow relative z-10">
