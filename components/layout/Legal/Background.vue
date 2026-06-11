9b4c0bd76b56ccd323c7801c6111dd0612b65f08
steveasterafovo <steveasterafovo@gmail.com>
2026-06-11 05:53:25 +0100

    feat: implement legal layout and index page with reusable navigation and styling components

---
+++ components/layout/Legal/Background.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- Background Decorators -->
+  <div class="absolute inset-0 z-0 overflow-hidden pointer-events-none">
+    <div class="absolute top-0 left-0 w-full h-full bg-[url('/img/grid-pattern.svg')] opacity-[0.03] dark:opacity-[0.02]"></div>
+    <div class="absolute top-0 right-0 w-[600px] h-[600px] bg-primary-500/8 rounded-full blur-[120px] -translate-y-1/3 translate-x-1/3"></div>
+    <div class="absolute bottom-0 left-0 w-[500px] h-[500px] bg-secondary-500/8 rounded-full blur-[100px] translate-y-1/3 -translate-x-1/3"></div>
+  </div>
+</template>
