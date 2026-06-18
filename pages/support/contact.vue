db8d04d274b65d974513ed0a54d1243d72ea69b8
steveasterafovo <steveasterafovo@gmail.com>
2026-06-18 02:52:18 +0100

    feat: implement contact page with inquiry form and support information components

--- pages/support/contact.vue
+++ pages/support/contact.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 font-sans transition-colors duration-300">
-    <RootHelpContactHero />
+    <RootSupportContactHero />
 
     <div class="container-custom -mt-20 pb-20 relative z-10">
       <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <RootHelpContactInfo />
-        <RootHelpContactForm />
+        <RootSupportContactInfo />
+        <RootSupportContactForm />
       </div>
     </div>
   </div>
