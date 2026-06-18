5fb705d12b13212e232a7849b33f00c7bb20e502
steveasterafovo <steveasterafovo@gmail.com>
2026-06-18 02:19:44 +0100

    refac index

--- pages/pricing/index.vue
+++ pages/pricing/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 font-sans transition-colors duration-300">
-    <RootPricingHeroSection :is-annual="isAnnual" @update:is-annual="isAnnual = $event" />
-    <RootPricingPlansSection :is-annual="isAnnual" />
-    <RootPricingFaqSection />
-    <RootPricingCtaSection />
+    <RootPricingHomeHero :is-annual="isAnnual" @update:is-annual="isAnnual = $event" />
+    <RootPricingHomePlans :is-annual="isAnnual" />
+    <RootPricingHomeFaq />
+    <RootPricingHomeCta />
   </div>
 </template>
 
