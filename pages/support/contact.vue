62c81735573d31c2f1a50e21a43f9a31adf9b937
steveasterafovo <steveasterafovo@gmail.com>
2026-06-18 02:42:58 +0100

    feat: create contact support page with form and info components

--- pages/support/contact.vue
+++ pages/support/contact.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 font-sans transition-colors duration-300">
-    <HelpContactHero />
+    <RootHelpContactHero />
 
     <div class="container-custom -mt-20 pb-20 relative z-10">
       <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <HelpContactInfo />
-        <HelpContactForm />
+        <RootHelpContactInfo />
+        <RootHelpContactForm />
       </div>
     </div>
   </div>
