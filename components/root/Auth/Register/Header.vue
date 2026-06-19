5f1375b626cdb344c1a0a5f297f33aa85210a0f1
steveasterafovo <steveasterafovo@gmail.com>
2026-06-20 00:45:06 +0100

    feat: implement user registration page and modular registration form components

---
+++ components/root/Auth/Register/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="mt-4 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+      Créer un compte
+    </h2>
+    <p class="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
+      Ou
+      <NuxtLink to="/auth/login" class="font-medium text-primary-600 hover:text-primary-500">
+        connectez-vous à votre compte existant
+      </NuxtLink>
+    </p>
+  </div>
+</template>
