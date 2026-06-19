a7d97abad72e44638639bdc52bfd98becabc01a8
steveasterafovo <steveasterafovo@gmail.com>
2026-06-19 23:33:54 +0100

    feat: implement authentication login page with role-based switching and credentials form

---
+++ components/root/Auth/Login/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="mt-4 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+      Connexion
+    </h2>
+    <p class="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
+      Ou
+      <NuxtLink to="/auth/register" class="font-medium text-primary-600 hover:text-primary-500">
+        créez un compte gratuitement
+      </NuxtLink>
+    </p>
+  </div>
+</template>
