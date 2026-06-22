4edba8042d673e6758321ad59434c33dc64072c2
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:49:28 +0100

    feat: implement help center pages and loading states for categories and articles

--- components/root/Support/Help/Category/Loading.vue
+++ components/root/Support/Help/Category/Loading.vue
@@ -3,7 +3,7 @@
     <UiAppSkeleton class="h-10 w-1/3 mb-4 rounded-xl" />
     <UiAppSkeleton class="h-6 w-2/3 mb-8 rounded-xl" />
     <div class="space-y-4">
-      <UiAppSkeleton class="h-24 w-full rounded-2xl" v-for="i in 1" :key="i" />
+      <UiAppSkeleton class="h-24 w-full rounded-2xl" v-for="i in 3" :key="i" />
     </div>
   </div>
 </template>
