46e2103ae7811a8fc9b766ce19145858262674f3
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:48:11 +0100

    feat: implement help center pages with dynamic routing and loading states

--- pages/support/help/[category]/index.vue
+++ pages/support/help/[category]/index.vue
@@ -3,11 +3,7 @@
     <RootSupportHelpCategoryBreadcrumb :category-title="category?.title" />
     <div class="card p-8 md:p-12">
       <template v-if="store.loading.help">
-        <UiAppSkeleton class="h-10 w-1/3 mb-4 rounded-xl" />
-        <UiAppSkeleton class="h-6 w-2/3 mb-8 rounded-xl" />
-        <div class="space-y-4">
-          <UiAppSkeleton class="h-24 w-full rounded-2xl" v-for="i in 3" :key="i" />
-        </div>
+        <RootSupportHelpCategoryLoading />
       </template>
       <template v-else>
         <RootSupportHelpCategoryHeader :category="category" />
