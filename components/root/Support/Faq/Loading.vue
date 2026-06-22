0ca7fcdef99ddafbaf254481da2b9ce6d16de212
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:41:36 +0100

    feat: implement support center pages including help documentation, category browsing, article details, and safety guidelines

---
+++ components/root/Support/Faq/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="space-y-6 mt-8">
+    <div v-for="i in 3" :key="i" class="space-y-3">
+      <UiAppSkeleton type="heading" width="55%" />
+      <UiAppSkeleton type="text" :count="2" gap="2" />
+    </div>
+  </div>
+</template>
