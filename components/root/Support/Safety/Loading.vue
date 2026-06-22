e6095ffb8d7071fb2f2e8ac2ba19b99348c542cf
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:43:53 +0100

    feat: implement help center support pages with dynamic routing and loading skeletons

--- components/root/Support/Safety/Loading.vue
+++ components/root/Support/Safety/Loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="space-y-6 mt-8">
-    <div v-for="i in 4" :key="i" class="flex gap-4">
+    <div v-for="i in 2" :key="i" class="flex gap-4">
       <UiAppSkeleton type="square" width="48px" height="48px" />
       <div class="flex-1 space-y-2">
         <UiAppSkeleton type="heading" width="40%" />
