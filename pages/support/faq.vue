475825d2524c294ab066c2a37f495e63bbd3df68
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:32:14 +0100

    feat: implement support pages including FAQ, help center, and safety information with associated loading states

--- pages/support/faq.vue
+++ pages/support/faq.vue
@@ -4,12 +4,7 @@
       <RootSupportFaqHeader />
 
       <!-- Loading State -->
-      <div v-if="store.loading.faqs" class="space-y-6 mt-8">
-        <div v-for="i in 3" :key="i" class="space-y-3">
-          <UiAppSkeleton type="heading" width="55%" />
-          <UiAppSkeleton type="text" :count="2" gap="2" />
-        </div>
-      </div>
+      <RootSupportFaqLoading v-if="store.loading.faqs" />
 
       <template v-else>
         <RootSupportFaqCategories :categories="categories" :activeCategory="activeCategory"
