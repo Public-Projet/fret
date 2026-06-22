3ccd331686413cfd0447c7834c44de59f4e238c0
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:49:07 +0100

    feat: implement support help center home and article detail pages with skeleton loading states

---
+++ components/root/Support/Help/Category/Loading.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <UiAppSkeleton class="h-10 w-1/3 mb-4 rounded-xl" />
+    <UiAppSkeleton class="h-6 w-2/3 mb-8 rounded-xl" />
+    <div class="space-y-4">
+      <UiAppSkeleton class="h-24 w-full rounded-2xl" v-for="i in 1" :key="i" />
+    </div>
+  </div>
+</template>
