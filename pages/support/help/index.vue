de05b43cee22ae62b01fdb7118cf5dff10f564d9
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 00:50:40 +0100

    feat: implement help center home and article detail pages with loading states

--- pages/support/help/index.vue
+++ pages/support/help/index.vue
@@ -9,15 +9,7 @@
       </template>
 
       <template v-else-if="store.loading.help">
-        <!-- Loading State -->
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-12 mt-8">
-          <div v-for="i in 6" :key="i"
-            class="p-6 bg-gray-50 dark:bg-gray-800/50 rounded-2xl border border-gray-200 dark:border-gray-700 space-y-3">
-            <UiAppSkeleton type="square" width="48px" height="48px" />
-            <UiAppSkeleton type="heading" width="60%" />
-            <UiAppSkeleton type="text" :count="2" gap="2" />
-          </div>
-        </div>
+        <RootSupportHelpHomeLoading />
       </template>
 
       <template v-else>
