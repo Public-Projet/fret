2aed6a268167a1be95f53254ff4b68065cb1f868
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 16:36:29 +0100

    Please provide the code changes or the files you are referring to so I can generate the commit message for you.

--- components/root/Annonce/Home/Results/index.vue
+++ components/root/Annonce/Home/Results/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Availabilities Grid -->
-    <AnnoncesAvailabilitiesList v-if="activeTab === 'avail'" :items="availItems"
+    <RootAnnonceAvailabilitiesList v-if="activeTab === 'avail'" :items="availItems"
       @open-rate-modal="$emit('openRateModal', $event)" />
 
     <!-- Fret Announcements Grid -->
