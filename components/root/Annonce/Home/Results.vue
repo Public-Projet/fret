3f506717dbd3562900d9141becc538fad573dfae
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 16:27:52 +0100

    feat: add results list component and rating modal for carrier reviews

--- components/root/Annonce/Home/Results.vue
+++ components/root/Annonce/Home/Results.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <!-- Availabilities Grid -->
-    <AnnoncesAvailabilitiesList
-      v-if="activeTab === 'avail'"
-      :items="availItems"
-      @open-rate-modal="$emit('openRateModal', $event)"
-    />
+    <AnnoncesAvailabilitiesList v-if="activeTab === 'avail'" :items="availItems"
+      @open-rate-modal="$emit('openRateModal', $event)" />
 
     <!-- Fret Announcements Grid -->
     <AnnoncesFretList v-else :items="fretItems" />
