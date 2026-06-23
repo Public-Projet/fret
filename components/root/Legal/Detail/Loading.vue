ced2123cad8f9d22612b2237083be58bb6c4bd40
steveasterafovo <steveasterafovo@gmail.com>
2026-06-23 01:44:14 +0100

    feat: implement legal pages and reusable loading/empty states for content display

---
+++ components/root/Legal/Detail/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="space-y-8">
+    <div v-for="i in 5" :key="i" class="space-y-3">
+      <UiAppSkeleton :loading="true" type="heading" width="45%" height="22px" />
+      <UiAppSkeleton :loading="true" type="text" :count="3" gap="2" />
+    </div>
+  </div>
+</template>
