2988eda92723ce2e687a8cf4982735df4f7b27b2
steveasterafovo <steveasterafovo@gmail.com>
2026-06-25 18:25:56 +0100

    feat: implement carrier profile page with subscription, fleet, and document management components

--- pages/app/uc/profile.vue
+++ pages/app/uc/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 pb-12">
     <!-- Header -->
-    <RootAppCarProfilCarrierHeader :profile="profile" />
+    <RootAppCarProfilHeader :profile="profile" />
 
     <!-- Stats Bar -->
     <ProfileCarrierStatsBar :missions="0" :rating="'-'" :vehicles="vehicles.length"
