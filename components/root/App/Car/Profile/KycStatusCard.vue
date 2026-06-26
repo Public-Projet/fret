2ef696114ac3d7ba72222bcc3ab895bb9b68c60d
steveasterafovo <steveasterafovo@gmail.com>
2026-06-26 01:46:01 +0100

    Car KYC2

--- components/root/App/Car/Profile/KycStatusCard.vue
+++ components/root/App/Car/Profile/KycStatusCard.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="bg-secondary-600 text-white rounded-xl p-6 relative overflow-hidden">
-    <UiAppSkeleton :loading="loading" type="card" theme="dark">
+    <!-- Chargement -->
+    <div v-if="loading" class="space-y-3">
+      <UiAppSkeleton :loading="true" type="text" theme="dark" class="w-1/2" />
+      <UiAppSkeleton :loading="true" type="text" theme="dark" />
+      <UiAppSkeleton :loading="true" type="text" theme="dark" class="w-1/4 mt-2" />
+    </div>
+
+    <!-- Contenu -->
+    <template v-else>
       <div class="relative z-10">
         <h4 class="font-bold mb-2">Besoin d'aide ?</h4>
         <p class="text-secondary-100 text-sm mb-2">Contactez le support pour mettre à jour vos documents.</p>
@@ -13,7 +21,7 @@
         <NuxtLink to="/support/contact" class="btn bg-white text-secondary-700 border-0 btn-sm">Support</NuxtLink>
       </div>
       <IconHeadset class="absolute -bottom-4 -right-4 w-24 h-24 text-white/10" />
-    </UiAppSkeleton>
+    </template>
   </div>
 </template>
 
